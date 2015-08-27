785bc5f244ad3898132766a8fa5794a2ee36b411
kurehajime <x.iidec+gh@googlemail.com>
2015-08-27 16:45:12 +0900

    utf-8 only

--- main.go
+++ main.go
@@ -2,16 +2,9 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"golang.org/x/text/encoding/japanese"
-	"golang.org/x/text/transform"
 	"io/ioutil"
 	"os"
-	"os/exec"
-	"regexp"
-	"runtime"
-	"strconv"
 )
 
 const (
@@ -21,19 +14,8 @@ const (
 
 func main() {
 	var text string
-	var err error
 	text, ok := readPipe()
-	if ok == true {
-		encode := getConsoleLang()
-		switch encode {
-		case SHIFT_JIS:
-			text, err = utf2sjis(text)
-			if err != nil {
-				fmt.Println(err.Error())
-				os.Exit(1)
-			}
-		}
-	} else {
+	if ok ==false {
 		text, ok = readFileByArg()
 		if ok == false {
 			os.Exit(1)
@@ -76,29 +58,4 @@ func readFileByArg() (string, bool) {
 	return string(content), true
 }
 
-//for Windows
-func getConsoleLang() int {
-	if runtime.GOOS != "windows" {
-		return UTF8 //default:utf-8
-	}
-	out, err := exec.Command("chcp").Output()
-	if err != nil {
-		fmt.Println(err)
-		return UTF8 //default:utf-8
-	}
-	rtn, err := strconv.Atoi(string(regexp.MustCompile(`(\d+)`).FindSubmatch([]byte(string(out)))[0]))
-	if err != nil {
-		fmt.Println(err)
-		return UTF8 //default:utf-8
-	}
-	return rtn
-}
 
-func utf2sjis(text string) (string, error) {
-	str, err := ioutil.ReadAll(transform.NewReader(bytes.NewReader([]byte(text)), japanese.ShiftJIS.NewDecoder()))
-	if err == nil {
-		return string(str), nil
-	} else {
-		return "", err
-	}
-}
