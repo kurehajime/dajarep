a2bff0994f9492a396524f9f9c6a520564b07789
kurehajime <x.iidec+gh@googlemail.com>
2015-08-27 18:27:50 +0900

    fix

--- dajarep.go
+++ dajarep.go
@@ -27,7 +27,7 @@ func Dajarep(text string) []string {
 	sentences := getSentences(text)
 	var dajares []string
 	for i := 0; i < len(sentences); i++ {
-		if isDajare(sentences[i]) == true {
+		if ok, _ := isDajare(sentences[i]); ok == true {
 			dajares = append(dajares, sentences[i].str)
 		}
 	}
@@ -35,11 +35,11 @@ func Dajarep(text string) []string {
 }
 
 //駄洒落かどうかを評価する。
-func isDajare(sen sentence) bool {
+func isDajare(sen sentence) (bool, string) {
 	words := sen.words
 	for i := 0; i < len(words); i++ {
 		w := words[i]
-		if w.wtype == "名詞" {
+		if w.wtype == "名詞" && len([]rune(w.kana)) > 1 {
 			r_str := regexp.MustCompile(w.str)
 			r_kana := regexp.MustCompile(fixWord(w.kana))
 			hit_str := r_str.FindAllString(sen.str, -1)
@@ -47,11 +47,11 @@ func isDajare(sen sentence) bool {
 			hit_kana2 := r_kana.FindAllString(fixSentence(sen.kana), -1)
 			//ある単語における　原文の一致文字列数<フリガナでの一致文字列数　→　駄洒落の読みが存在
 			if len(hit_str) < int(math.Max(float64(len(hit_kana)), float64(len(hit_kana2)))) {
-				return true
+				return true, w.str
 			}
 		}
 	}
-	return false
+	return false, ""
 }
 
 //置き換え可能な文字を考慮した正規表現を返す。
--- main.go
+++ main.go
@@ -30,6 +30,7 @@ func main() {
 	for i := 0; i < len(s); i++ {
 		fmt.Println(s[i])
 	}
+
 }
 
 func readPipe() (string, bool) {
