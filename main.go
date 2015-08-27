e8631185afab5db8d64b2c947d5c21a4b1bf6e9a
kurehajime <x.iidec+gh@googlemail.com>
2015-08-27 17:51:13 +0900

    support sjis

--- main.go
+++ main.go
@@ -2,26 +2,30 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/transform"
 	"io/ioutil"
 	"os"
 )
 
-const (
-	UTF8      = 65001
-	SHIFT_JIS = 932
-)
-
 func main() {
 	var text string
+	var err error
 	text, ok := readPipe()
-	if ok ==false {
+	if ok != true {
 		text, ok = readFileByArg()
 		if ok == false {
 			os.Exit(1)
 		}
 	}
-
+	//encode
+	text, err = transEnc(text)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+	}
+	//dajarep
 	s := Dajarep(text)
 	for i := 0; i < len(s); i++ {
 		fmt.Println(s[i])
@@ -58,4 +62,30 @@ func readFileByArg() (string, bool) {
 	return string(content), true
 }
 
-
+//「Golangで文字コード判定」qiita.com/nobuhito/items/ff782f64e32f7ed95e43
+func transEnc(text string) (asset string, err error) {
+	body := []byte(text)
+	var f []byte
+	encodings := []string{"sjis", "utf-8"}
+	for _, enc := range encodings {
+		if enc != "" {
+			ee, _ := charset.Lookup(enc)
+			if ee == nil {
+				continue
+			}
+			var buf bytes.Buffer
+			ic := transform.NewWriter(&buf, ee.NewDecoder())
+			_, err := ic.Write(body)
+			if err != nil {
+				continue
+			}
+			err = ic.Close()
+			if err != nil {
+				continue
+			}
+			f = buf.Bytes()
+			break
+		}
+	}
+	return string(f), nil
+}
