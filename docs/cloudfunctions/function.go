0c5fc692cf6eae29553ecf116fb2e97ca07575bb
kurehajime <xiidec@gmail.com>
2022-07-16 22:42:36 +0900

    Playgroundを追加

---
+++ docs/cloudfunctions/function.go
@@ -0,0 +1,43 @@
+// Package p contains an HTTP Cloud Function.
+package p
+
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+
+	"github.com/kurehajime/dajarep"
+)
+
+func Dajareper(w http.ResponseWriter, r *http.Request) {
+	var d struct {
+		Messages []string `json:"messages"`
+	}
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	input := strings.Join(d.Messages, "\n")
+	dajares, _ := dajarep.Dajarep(input)
+	d.Messages = dajares
+	out, err := json.Marshal(d)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	// Set CORS headers for the main request.
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json,;charset=UTF-8")
+	w.Write(out)
+}
---
+++ docs/cloudfunctions/go.mod
@@ -0,0 +1,11 @@
+module example.com/cloudfunction
+
+go 1.16
+
+require github.com/kurehajime/dajarep v1.9.5
+
+require (
+	github.com/ikawaha/kagome-dict v1.0.2 // indirect
+	github.com/ikawaha/kagome-dict/ipa v1.0.2 // indirect
+	github.com/ikawaha/kagome/v2 v2.4.4 // indirect
+)
