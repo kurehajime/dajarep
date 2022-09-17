29922cdc057df55970fba71a1153a3cb2223e8df
kurehajime <xiidec@gmail.com>
2022-09-17 21:16:51 +0900

    さすがにCORSはちゃんとする

--- docs/cloudfunctions/function.go
+++ docs/cloudfunctions/function.go
@@ -13,9 +13,16 @@ func Dajareper(w http.ResponseWriter, r *http.Request) {
 	var d struct {
 		Messages []string `json:"messages"`
 	}
+	var allows = []string{"http://127.0.0.1:8080", "https://kurehajime.github.io"}
+	for _, v := range allows {
+		if v == r.Header.Get("Origin") {
+			w.Header().Set("Access-Control-Allow-Origin", v)
+			break
+		}
+	}
+
 	// Set CORS headers for the preflight request
 	if r.Method == http.MethodOptions {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		w.Header().Set("Access-Control-Max-Age", "3600")
